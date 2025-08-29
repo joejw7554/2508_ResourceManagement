3c33bfa04cbfc7781f29ec6485c65937971df041
JaeWoo Joe <joejw7554@gmail.com>
2025-08-29 11:11:53 +0900

    Implementation: Matain Data between Scene to Scene
    Used Json to load and save file through Unity.

---
+++ Junior-Programmer-Starter-Files/Assets/Scripts/MainManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.IO;
+
+
+public class MainManager : MonoBehaviour
+{
+
+    public static MainManager Instance;
+
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+
+        LoadColor();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if(File.Exists(path))
+        {
+            string json= File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+
+
+
+
+}
